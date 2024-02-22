288e27977ae6cadb6957119a18d5c2877db237e8
Aaron <aarondivi@gmail.com>
2024-02-22 15:24:09 +0100

    init

--- frontend/src/public/components/Contact/index.jsx
+++ frontend/src/public/components/Contact/index.jsx
@@ -1,11 +1,13 @@
-import { useParams, Navigate } from 'react-router-dom'
+import { useParams, Navigate, useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
-import biensArray from '../../datas/biensArray.json'
 import pagesHeadersArray from '../../datas/pagesHeadersArray.json'
 import Footer from '../Footer'
 import HeaderBanner from '../HeaderBanner'
 import woman from '../../assets/pictures/woman.png'
 import ContactForm from '../../../functions/ContactForm'
+import biensArray from '../../datas/biensArray.json'
+import { faPrint, faEnvelope, faArrowCircleLeft } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 // styles
 import './m-contact.css'
@@ -21,12 +23,27 @@ const Contact = () => {
     const contactHeaderArray = []
     contactHeaderArray.push(contactHeader)
 
-    const isVenteContact = card.Opérations.includes("Vente")
-    const isLocationContact = card.Opérations.includes("Location")
+    const isAchatContact = card.Opérations.includes('Achat')
+    const isVenteContact = card.Opérations.includes('Vente')
+    const isLocationContact = card.Opérations.includes('Location')
 
     useEffect(() => {
         document.documentElement.scrollTo({ top: 0, behavior: 'smooth' })
     }, [])
+
+    const navigate = useNavigate();
+
+
+    const allOpérations = ['Achat', 'Location', 'Location saisonnière', 'Programme', 'Viager', 'Enchère']
+    const isAnyOperationIncluded = allOpérations.some(operation => card.Opérations.includes(operation))
+
+    const retour = () => {
+        if (isAnyOperationIncluded) {
+            navigate(`/BiensCard/${Id}`)
+        } else {
+            navigate('/')
+        }
+    }
 
 	return (
         <>
@@ -35,42 +52,81 @@ const Contact = () => {
                     <HeaderBanner />
                     <h1 className='headerBanner__header__message'>Contact</h1>
                     <div className='content-contact'>
-                    <div className='containerContact'>
-                        <div className='containerContact__text' >
-                            {isVenteContact &&
-                                <p className='containerContact__text__header'>
-                                    Estimation offerte
-                                </p>
-                            }
-                            {isLocationContact &&
-                                <p className='containerContact__text__header'>
-                                    Location
-                                </p>
-                            }
-                            <div className='containerContact__text__form'>
-                                <ContactForm />
+                        {(isAchatContact || isVenteContact || isLocationContact) && 
+                            <ul className='biensCard__content__tag'>
+                                {
+                                    (
+                                        card.Exclu !== false && card.Exclu !== "" &&
+                                        card.Exclu !== undefined
+                                    ) &&
+                                    <li className='biensCard__content__tag__exclu'>Exclusivité</li>
+                                } 
+                                <li>{card.Opérations}</li>
+                                <li>{card.TypesB}</li>
+                                <li>{card.NombreP}</li>
+                                <li>{card.Localisations}</li>
+                                <li 
+                                    id='biensCard__content__tag__retour'
+                                    onClick={retour}
+                                >
+                                    Retour&nbsp;
+                                    <FontAwesomeIcon
+                                        icon={faArrowCircleLeft}
+                                    />
+                                </li>
+                                <li id='biensCard__content__tag__envoyer' title='Envoyer à un ami'>
+                                    <FontAwesomeIcon
+                                        icon={faEnvelope}
+                                    />
+                                </li>
+                                <li id='biensCard__content__tag__imprimer' title='Imprimer cette page'>
+                                    <FontAwesomeIcon
+                                        icon={faPrint}
+                                    />
+                                </li>
+                            </ul>
+                        }
+                        <div className='containerContact'>
+                            <div className='containerContact__text' >
+                                {isAchatContact &&
+                                    <p className='containerContact__text__header'>
+                                        Achat ref : {card.RefNum}
+                                    </p>
+                                }
+                                {isVenteContact &&
+                                    <p className='containerContact__text__header'>
+                                        Estimation offerte
+                                    </p>
+                                }
+                                {isLocationContact &&
+                                    <p className='containerContact__text__header'>
+                                        Location ref : {card.RefNum}
+                                    </p>
+                                }
+                                <div className='containerContact__text__form'>
+                                    <ContactForm />
+                                </div>
                             </div>
+                            <div className='containerContact__bloc'>
+                                <img 
+                                    className='containerContact__bloc__woman' 
+                                    src={woman}
+                                    width='1130px'
+                                    height='2078px'
+                                    fetchpriority='high' 
+                                    alt='serrage de main'
+                                />
+                            </div>
+                            <p className='containerContact__bloc__pub'>
+                                La clé de vos 
+                                <br/>
+                                projets immo
+                                <br/>
+                                est à portée 
+                                <br/>
+                                de main
+                            </p>
                         </div>
-                        <div className='containerContact__bloc'>
-                            <img 
-                                className='containerContact__bloc__woman' 
-                                src={woman}
-                                width='1130px'
-                                height='2078px'
-                                fetchpriority='high' 
-                                alt='serrage de main'
-                            />
-                        </div>
-                        <p className='containerContact__bloc__pub'>
-                            La clé de vos 
-                            <br/>
-                            projets immo
-                            <br/>
-                            est à portée 
-                            <br/>
-                            de main
-                        </p>
-                    </div>
                     </div>
                 </section>
                 : 
